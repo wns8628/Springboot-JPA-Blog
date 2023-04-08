a3f19bac79a36b6f8db843e13c048a3146516673
김세준 <kimsejun@gimsejun-ui-MacBookPro.local>
2023-04-08 13:31:34 +0900

    환경세팅완료 v1

---
+++ src/test/java/com/cost/blogTest/BlogTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cost.blogTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
